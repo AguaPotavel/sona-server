8da81b00cdf3f2cb1a5064089dfa261d8d466775
Ruan Carlos <ruancarlosmeira@gmail.com>
2024-03-09 18:57:01 -0300

    Adiciona ícone de favicon ao layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ import { NextAuthProvider } from "./_shared/hooks/providers/provider";
 export const metadata: Metadata = {
   title: "Sona",
   description: "Seu app de voice",
+  icons: [
+    { rel: "icon", url: "/favicon.ico" }
+  ]
 };
 
 export default function RootLayout({
