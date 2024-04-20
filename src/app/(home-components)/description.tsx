bc48f3b3839076274a870aa48be7739bac3fc8a4
Ruan Carlos <ruancarlosmeira@gmail.com>
2024-04-19 21:20:10 -0300

    feat: added canvas and model laptop 3D

--- src/app/(home-components)/description.tsx
+++ src/app/(home-components)/description.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { Suspense, useEffect } from "react";
 
-import { Button } from '../_shared/components/ui/button'
+import { Button } from "../_shared/components/ui/button";
+import CanvasComponent from "./canvas/canvasComponent";
+import useMeasure from "react-use-measure";
 
 export default function HomeDescription() {
+  const [ref, { width, height }] = useMeasure();
+
+  useEffect(() => {
+    console.log(width, height);
+  }, [ref]);
+
   return (
-    <section className='h-svh w-full items-center flex flew-row px-14 xl:px-24 gap-10'>
-        <div className='text-2xl flex flex-1 flex-col px-4 lg:mt-36 lg:text-6xl'>
-            <span>
-                <strong className='text-indigo-500'>SONA</strong>, 
-            dando voz
-            </span>
-            <span>as suas <strong className='text-primary-500'>Rankeadas</strong>!</span>
-            <span className='text-sm lg:text-3xl'>Deixe os pings pra tras!</span>
+    <section className="h-svh w-full items-center flex flew-row px-14 xl:px-24 gap-10">
+      <div className="text-2xl flex flex-1 flex-col px-4 lg:mt-36 lg:text-6xl">
+        <span>
+          <strong className="text-indigo-500">SONA</strong>, dando voz
+        </span>
+        <span>
+          as suas <strong className="text-primary-500">Rankeadas</strong>!
+        </span>
+        <span className="text-sm lg:text-3xl">Deixe os pings pra tras!</span>
 
-            <Button className='w-40 mt-4' variant={'secondary'}>Download Grátis</Button>
-        </div>
+        <Button className="w-40 mt-4" variant={"secondary"}>
+          Download Grátis
+        </Button>
+      </div>
 
-        <div className='hidden text-6xl flex-1 flex flex-col px-4 lg:mt-36 lg:flex'>
-            
-        </div>
+      <div
+        ref={ref}
+        className="hidden text-6xl min-h-[600px] max-w-[800px] flex-1 flex flex-col items-center justify-center px-4 lg:mt-36 lg:flex"
+      >
+        <Suspense fallback={<div>loading</div>}>
+          <CanvasComponent sizes={{ width: width, height: height }} />
+        </Suspense>
+      </div>
     </section>
-  )
+  );
 }
