e7a918243e387a4eb9dd0edc9ba4f64c46cf565d
Ruan Carlos <ruancarlosmeira@gmail.com>
2024-04-23 19:07:42 -0300

    style: update landing page style

--- src/app/(home-components)/description.tsx
+++ src/app/(home-components)/description.tsx
@@ -1,9 +1,10 @@
-import React, { Suspense, useEffect } from "react";
+import React from "react";
 
 import { Button } from "../_shared/components/ui/button";
 import HomeAnimation from "./homeAnimation";
-import CanvasComponent from "./canvas/canvasComponent";
 import useMeasure from "react-use-measure";
+
+import { motion } from "framer-motion"
 
 export default function HomeDescription() {
   const [ref, { width, height }] = useMeasure();
@@ -13,29 +14,25 @@ export default function HomeDescription() {
   };
 
   return (
-    <section className="h-svh w-full justify-center items-center flex flew-row px-14 xl:px-24 gap-10">
-      <div className="text-4xl flex flex-1 flex-col px-4 lg:mt-36 lg:text-6xl">
-        <span className="text-slate-300">
-          <strong className="text-indigo-500">SONA</strong>, dando voz
+    <section className="h-svh w-full justify-center items-center flex flew-row px-14 lg: xl:px-24 gap-10 font-body">
+      <motion.div initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0, transition: { delay: 0, duration: .3}}} className="text-5xl justify-center flex flex-1 flex-col px-4 lg:text-6xl">
+        <span className="text-slate-50">
+          <strong className="text-indigo-500 font-display bg-slate-50 px-2 mr-2 lg:text-8xl">Sona</strong>, dando voz
         </span>
-        <span className="text-slate-300">
-          as suas <strong className="text-slate-100">Rankeadas</strong>!
+        <span className="text-slate-50 leading-relaxed">
+          as suas <strong className="text-slate-100 text-5xl lg:text-6xl bg-indigo-500 px-2 mr-2">Rankeadas!</strong>
         </span>
-        <span className="text-xl lg:text-3xl text-slate-500">Deixe os pings pra tras!</span>
+        <span className="text-xl lg:text-3xl text-indigo-500 mb-10 lg:mb-4">Deixe os pings pra tras!</span>
 
-        <Button className="w-40 mt-4" variant={"secondary"} onClick={handleDownloadLastVersion}>
+        <Button className="lg:w-40 " variant={"default"} onClick={handleDownloadLastVersion}>
           Download Grátis
         </Button>
-      </div>
+      </motion.div>
 
       <div
         ref={ref}
-        className="hidden text-6xl min-h-[600px] max-w-[800px] flex-1 flex flex-col items-center justify-center px-4 lg:mt-36 lg:flex"
+        className="hidden text-6xl min-h-[600px] max-w-[800px] flex-1 flex flex-col items-center justify-center px-4 lg:flex"
       >
-        {/* <Suspense fallback={<div className="text-indigo-500">carregando...</div>}>
-          <CanvasComponent sizes={{ width: width, height: height }} />
-        </Suspense> */}
-
         <HomeAnimation />
       </div>
     </section>
